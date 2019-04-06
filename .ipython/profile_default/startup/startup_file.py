f3e4d3ad6c8b07250f406a403b3fc1d06f0376ff
Mpho Mphego <mpho112@gmail.com>
2019-04-06 09:27:55 +0200

    Updated ipython startupfile

--- .ipython/profile_default/startup/startup_file.py
+++ .ipython/profile_default/startup/startup_file.py
@@ -1,28 +1,17 @@
-import time
-import sys
 import os
+import subprocess
+import sys
+import time
 
 
 class Style:
-    RED = '\033[91m'
-    BOLD = '\033[1m'
-    END = '\033[0m'
+    BOLD = "\033[1m"
+    END = "\033[0m\n"
+    RED = "\033[91m"
+    LINE = "\n"
 
-errmsg = ''
 
-try:
-    import matplotlib.pyplot as plt
-except Exception as _exp:
-    errmsg += "{}ERROR:{} {}\n".format(Style.RED, Style.END, str(_exp))
-try:
-    import numpy as np
-except Exception as _exp:
-    errmsg += "{}ERROR:{} {}\n".format(Style.RED, Style.END, str(_exp))
-
-msg = (
-    "{}---> Automagically imported these packages (if available): time, sys, os, plt and np {}".format("".join([Style.BOLD, Style.RED]), Style.END))
-try:
-    assert errmsg
-    print("\n{}\n{}".format(errmsg, msg))
-except Exception:
-    print(msg)
+modulenames = ", ".join(list(set(sys.modules) & set(globals())))
+msg = "---> Automagically imported these packages (if available): {}".format(modulenames)
+formatted_msg = Style.LINE + Style.BOLD + Style.RED + msg + Style.END
+print(formatted_msg)
