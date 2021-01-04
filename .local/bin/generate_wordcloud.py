5229049a9afdf62a848286ba63d930b5257c2c52
Mpho Mphego <mpho112@gmail.com>
2021-01-04 11:25:20 +0200

    ran black on wordcloud generator
    Signed-off-by: Mpho Mphego <mpho112@gmail.com>

--- .local/bin/generate_wordcloud.py
+++ .local/bin/generate_wordcloud.py
@@ -7,7 +7,29 @@ import random
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS
 
-stopwords = STOPWORDS.update(["self", 'item', 'refdef', 'http', 'https', 'null'])
+stopwords = STOPWORDS.update(
+    [
+        "div",
+        "http",
+        "https",
+        "image",
+        "images",
+        "item",
+        "jpg",
+        "null",
+        "png",
+        "post",
+        "refdef",
+        "review",
+        "self",
+        'githubusercontent',
+        'help',
+        'http',
+        'https',
+        'style',
+        'user-images',
+    ]
+)
 
 # def random_color_func(
 #     word, font_size, position, orientation, random_state=None, **kwargs
@@ -35,23 +57,23 @@ def main():
 
     with open(args.get("filename")) as _f:
         contents = [i.strip() for i in _f.readlines()]
-        contents = ' '.join(set(' '.join(set(contents)).split()))
+        contents = " ".join(set(" ".join(set(contents)).split()))
 
     if contents:
         wordcloud = WordCloud(
             width=900,
             height=200,
             stopwords=stopwords,
-            background_color ='black',
+            background_color="black",
             collocations=False,
             # https://matplotlib.org/users/colormaps.html
-            colormap='hsv',
+            colormap="hsv",
             random_state=21,
             max_words=300,
-            min_font_size=5
+            min_font_size=5,
         ).generate(contents)
         # wordcloud.recolor(color_func=random_color_func)
-        fig = plt.figure(figsize=[6,6], facecolor=None)
+        fig = plt.figure(figsize=[6, 6], facecolor=None)
         ax = fig.add_subplot(111)
         ax.imshow(wordcloud, interpolation="bilinear")
         ax.axes.get_xaxis().set_visible(False)
@@ -59,7 +81,10 @@ def main():
         ax.set_frame_on(False)
         ax.axis("off")
         location = pathlib.Path(args.get("save_location")).absolute()
-        plt.savefig(location, dpi=300, transparent=True, bbox_inches='tight', pad_inches=0)
+        plt.savefig(
+            location, dpi=300, transparent=True, bbox_inches="tight", pad_inches=0
+        )
+
 
 if __name__ == "__main__":
     main()
